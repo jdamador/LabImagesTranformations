a7844a191dbc63f4e129883e5e82e95555c2082a
Daniel Amador <jdamadorsalas@gmail.com>
2020-09-20 17:17:11 -0600

    feat:  add menu

--- scripts/functions.py
+++ scripts/functions.py
@@ -8,16 +8,20 @@ class Transformations:
         self.imagePath = imagePath
         self.img = cv2.imread(imagePath)
         
-    #? Deploy a RGB for an specific pixel given by the user.
+    #? 1) Deploy a RGB for an specific pixel given by the user.
     def getPixelRGB(self, posX, posY):
         (B, G, R) = self.img[posX, posY]
         print("These are the values for [R, G, B] composition for the specific pixel checked: \nR = {}, G = {} and B = {}".format(R, G, B))
 
-    #? Extract a region of interest using coordinates given by the user.
+    #? 2) Extract a region of interest using coordinates given by the user.
     def getROI(self, init, end):
         roi = self.img[init[0]: init[1], end[0]:end[1]];
         self.showImg('ROI Request', roi)
-    def getImageResize(self,):
+    
+    #? 6) Smoothing an image using Gaussian Blur
+    def getGaussianBlurImage(self):
+        blurred = cv2.GaussianBlur(self.img, (11, 11), 0)
+        self.showImg('Smoothing Image', blurred)
 
     #? Simple method to show a image.
     def showImg(self, label, image):
