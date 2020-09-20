ada2de42768aa3649ed062b04d0057c7e1ef857f
brandon <brandoncb9906@gmail.com>
2020-09-20 16:52:51 -0600

    Metodos 3,4 y 5

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,6 +1,8 @@
 
 # Import libraries
 import cv2
+import imutils as imutils
+
 
 class Transformations:
     #? Constructor
@@ -17,7 +19,25 @@ class Transformations:
     def getROI(self, init, end):
         roi = self.img[init[0]: init[1], end[0]:end[1]];
         self.showImg('ROI Request', roi)
-    def getImageResize(self,):
+
+    #? Allow to adjust the size of an image requesting a new
+    #  dimension in pixels and display the resulting image
+    def getImageResize(self, image, pixels):
+        resized = cv2.resize(image, (pixels, pixels))
+        self.showImg('Fixed Resizing', resized)
+        cv2.waitKey(0)
+
+    #? Resize an image but without altering the appearance
+    def getImageResizeWithoutAltheringAppearance(self, image):
+        resized = imutils.resize(image, width=300)
+        self.showImg("Imutils Resize", resized)
+        cv2.waitKey(0)
+
+    #? Rotate an image the number of degrees required
+    def getImageRotation(self, image, degrees):
+        rotated = imutils.rotate(image, degrees)
+        self.showImg("Imutils Rotation", rotated)
+        cv2.waitKey(0)
 
     #? Simple method to show a image.
     def showImg(self, label, image):
